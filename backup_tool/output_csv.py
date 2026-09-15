087444dd49810f2436f560997bf2276969c96ae3
peluviini <aptenodytesforsteri2020@au.com>
2026-09-15 12:07:09 +0900

    impl csv output class copied from pushSample_csv&&dump.py

---
+++ backup_tool/output_csv.py
@@ -0,0 +1,42 @@
+
+# libraries
+import os
+import csv
+from pathlib import Path
+
+# self-implementations
+from backup_tool.mysql_source import MySQLSource
+
+class CsvExport(MySQLSource):
+
+    def __init__(
+            self,
+            config,
+            table,
+            directory,
+        ):
+        super().__init__(
+            config,
+        )
+        self._table = table
+        self._directory = directory
+    
+    def do_output(self):
+
+    # this impl is cpied from 'pushSample_csv&&dump.py'
+    # of lines kinda 144 ~ 187
+        os.makedirs(f"{self._directory}/data", exist_ok=True)
+        with open(
+            f"{self._directory}/data/{self._table}.csv",
+            mode='w', newline='', encoding='utf-8'
+        ) as csvfile:
+            writer = csv.writer(csvfile)
+
+            column_names = self.get_column_types(table_name=self._table)
+            writer.writerow(column_names)
+
+            for raw_iter in self.iter_row_batches(self._table):
+                self.debug(raw_iter[0])
+                writer.writerow(raw_iter[0])
+                
+                self.debug(f"{self._table}テーブルのデータをCSVファイルに書き込みました。")
